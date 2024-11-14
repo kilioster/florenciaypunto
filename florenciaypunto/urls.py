9e227ac5ff256a41d46aa66616d1937cb4978aef
cristian farfan <cfarfansepulveda@gmail.com>
2024-11-14 13:31:56 -0300

    prueba

--- florenciaypunto/urls.py
+++ florenciaypunto/urls.py
@@ -13,3 +13,6 @@ urlpatterns = [
     path('news/', include('newsApp.urls')),
     path('newshist/', viewsnews.NewsHistory, name="newshistory")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
