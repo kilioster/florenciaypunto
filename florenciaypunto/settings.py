fce585b52a03ef261a9da6ea4457fb052515d3b5
cristian farfan <cfarfansepulveda@gmail.com>
2024-11-15 14:39:00 -0300

    cambios

--- florenciaypunto/settings.py
+++ florenciaypunto/settings.py
@@ -115,6 +115,9 @@ STATICFILES_DIRS = [
     
 ]
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # Donde Django almacenará los archivos estáticos al ejecutar collectstatic
+
+
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR /'media'
 
